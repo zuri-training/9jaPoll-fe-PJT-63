9dafbc7202b86d7eb789d9b0f93c7c287a0a6163
Prince @zu <azuokekeprince92@gmail.com>
2021-07-15 21:07:47 +0100

    Upadated Hamburger Menu

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,81 +1,68 @@
 // UI Variables
-const body = document.querySelector("body");
-const menu = document.querySelector(".nav-menu");
-const openBtn = document.querySelector(".open-icon");
+const menu = document.querySelector(".nav__wrapper");
+const toggleBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-icon");
 const menuOverlay = document.querySelector(".menu-overlay");
 const mediaSize = 861;
 
 
-function fetchInfo(query) {
-    const url = `https://poll9ja.herokuapp.com/api/candprofile`;
+// Toggle Menu
+toggleBtn.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    menuOverlay.classList.toggle("show");
+    document.body.classList.toggle("disabledScroll");
+  });
 
-    fetch(url).then(response => response.json()).then((jsonData) => {
-        console.log(jsonData);
-    });
-
-}
-
-
-window.onload = () => {
-    const searhBar = document.getElementById("searchField");
-    searhBar.onkeyup = (e) => {
-        fetchInfo(searhBar.value);
-    }
-}
-
-
-
-// Toggle Nav
-const toggleNav = () => {
-    menu.classList.toggle("open");
-    menuOverlay.classList.toggle(".active");
-    body.classList.toggle("disabledScroll");
-    console.log("click");
-}
-
-// Collapse Sub Menu
-const collapseSubMenu = () => {
-    menu.querySelector("sub-menu-parent.active .sub-menu").removeAttribute("style");
-    menu.querySelector("sub-menu-parent.active").classList.remove("active");
-
-}
-
-loadEventListeners = () => {
-    // Open Nav
-    openBtn.addEventListener("click", toggleNav);
-
-    // Close Nav
-    closeBtn.addEventListener("click", toggleNav);
-
-    // Close Nav by clicking outside
-    menuOverlay.addEventListener("click", toggleNav);
-
-    menu.addEventListener("click", (e) => {
-        if (e.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
-            const subMenuParent = e.target.parentElement;
-
-            // Collapse Sub Menu if already Expanded
-            if (subMenuParent.classList.contains("active")) {
-                collapseSubMenu();
-            }
-
-            // Collapse Existing Open Sub Menu
-            if (menu.querySelector(".sub-menu-parent.active")) {
-                collapseSubMenu();
-            }
-
-            // Expand New Sub Menu
-            subMenuParent.classList.add("active");
-            const subMenu = subMenuParent.querySelector(".sub-menu");
-            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+// Sub Menu Event Listener
+menu.addEventListener("click", (e) => {
+    if (e.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
+        const subMenuParent = e.target.parentElement;
+        // Collapse Sub Menu if already Expanded
+        if (subMenuParent.classList.contains("show")) {
+            collapseSubMenu();
         }
-    })
+        // Collapse Existing Open Sub Menu
+        if (menu.querySelector(".sub-menu-parent.show")) {
+            collapseSubMenu();
+        }
+        // Expand New Sub Menu
+        subMenuParent.classList.add("show");
+        const subMenu = subMenuParent.querySelector(".sub-menu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+    } 
 
-    console.log("click");
+    function collapseSubMenu() {
+        menu.querySelector("sub-menu-parent.show .sub-menu").removeAttribute("style");
+        menu.querySelector("sub-menu-parent.show").classList.remove("show");
+    }
 
-}
 
-loadEventListeners();
+
+    // if (e.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
+    //     const subMenuParent = e.target.parentElement;
+    //     // Collapse Sub Menu if already Expanded
+    //     if (subMenuParent.classList.contains("active")) {
+    //         collapseSubMenu();
+    //     }
+    //     // Collapse Existing Open Sub Menu
+    //     if (menu.querySelector(".sub-menu-parent.active")) {
+    //         collapseSubMenu();
+    //     }
+    //     // Expand New Sub Menu
+    //     subMenuParent.classList.add("active");
+    //     const subMenu = subMenuParent.querySelector(".sub-menu");
+    //     subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+    // }
+})
+
+
+
+
+
+
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Project/Product Website
+https://fervent-poincare-123986.netlify.app/
+
 # 9jaPoll-fe-PJT-63
 Election season is fast appproaching and 9japoll can be the number 1 stop websites where everyone can come and check the profile of those politicians contesting for executive offices or any key ranking posts in the country. This repo is meant to contain the front-end code for the 9jaPoll project by team pjt-63.
 
@@ -8,3 +11,6 @@ https://www.figma.com/files/project/32509717/9ja-Poll?fuid=955128788300862948
 
 ## Project documentation / Project Outline
 https://docs.google.com/spreadsheets/d/1pQUVwPvJ0tGgZ82vIIfS4DtxeI8BWSjCEoP_P7r0GSc/edit?usp=sharing
+
+## Backend API Link
+https://poll9ja.herokuapp.com/api/candprofile
