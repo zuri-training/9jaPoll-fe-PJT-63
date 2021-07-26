5a61ee6be345f3fb23d5886ebbed02a2fb91440f
Prince @zu <azuokekeprince92@gmail.com>
2021-07-26 12:14:59 +0100

    Updated Hamburger and Searh Bar functionality

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,6 +6,49 @@ const menuOverlay = document.querySelector(".menu-overlay");
 const mediaSize = 861;
 
 
+// Fetch Data foe Search Bar
+const getData = async (url) => {
+    const res = await fetch(url);
+    const json = await res.json();
+    console.log(json);
+
+    let candName = document.getElementById("candidate").value;
+    // let jsonCand = json.cand_name;
+
+
+    json.forEach((each, index) => {
+        if (each.cand_name == candName) {
+            let id = index;
+            localStorage.setItem("value", id);
+
+            let val = localStorage.getItem("value");
+            console.log(id);
+            window.location.href = "media and individual candidate page/presidential.html";
+
+    }
+
+});
+}
+
+
+const getList = async (url) => {
+    const res = await fetch(url);
+    const json = await res.json();
+    console.log(json);
+
+    json.forEach(each => {
+
+        document.getElementById("cand").innerHTML += `<option value="${each.cand_name}">`;
+        
+    });
+    
+};
+
+const url = "https://poll9ja.herokuapp.com/api/candprofile";
+getList(url);
+
+
+
 // Toggle Menu
 toggleBtn.addEventListener("click", function () {
     menu.classList.toggle("active");
@@ -36,23 +79,6 @@ menu.addEventListener("click", (e) => {
         menu.querySelector("sub-menu-parent.show").classList.remove("show");
     }
 
-
-
-    // if (e.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
-    //     const subMenuParent = e.target.parentElement;
-    //     // Collapse Sub Menu if already Expanded
-    //     if (subMenuParent.classList.contains("active")) {
-    //         collapseSubMenu();
-    //     }
-    //     // Collapse Existing Open Sub Menu
-    //     if (menu.querySelector(".sub-menu-parent.active")) {
-    //         collapseSubMenu();
-    //     }
-    //     // Expand New Sub Menu
-    //     subMenuParent.classList.add("active");
-    //     const subMenu = subMenuParent.querySelector(".sub-menu");
-    //     subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-    // }
 })
 
 
